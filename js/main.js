d18f28ef782b03b5b3535f14f701093a35dc9e10
xtornasol512 <xtornasol512@gmail.com>
2014-07-06 17:59:57 -0500

    ejemplo basico de leer archivos json, clicks on jquery  y funciones basicas

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var words = [];
+$(document).ready(function() {
+    
+    $("#uno").click(function() {
+      leerArchivo("json/cancion1.json", 1)
+      
+    });
+    $("#dos").click(function() {
+      leerArchivo("json/cancion2.json", 2)
+    });
+    $("#tres").click(function() {
+      leerArchivo("json/cancion3.json", 3)
+    });
+});
+function averiguar(cancionNum){
+    if (cancionNum == 1){
+
+        if ($("#palabra1").val() == words[0]){
+            $("#palabra1").removeClass("error");
+            alert( "Respuesta correcta :D " );
+
+        }
+        else{
+            alert( "Error en respuesta :S " );
+            $("#palabra1").addClass("error");
+        }
+
+    }
+    else if (cancionNum == 2){
+        
+        $("#palabra1").removeClass("error");
+        alert( "Haz escogido la cancion 2" );
+
+    }
+    else if (cancionNum == 3){
+        
+        $("#palabra2").addClass("error");
+    alert( "Haz escogido la cancion 3" );
+
+    }
+    else 
+        alert( "Recarga de nuevo porfavor" );
+
+}
+function leerArchivo(nombreArchivo, numCancion){
+    $.getJSON( nombreArchivo , function( data ) {
+      words = [];
+      $.each(data.cancion, function(indice, nombre){
+        console.log(' >', indice + '.' + nombre);
+        words[indice] = nombre;
+      });
+      
+    })
+    //es necesario usar el metodo .done para que analize cuando termine de crear el arreglo
+    //sino luego salen errores aunque sea verdadero
+    .done(function() {
+        averiguar(numCancion);
+    });
+}
